ab0327c2511f8c0688f2648b485f4fa1c8db1169
Sibasis Nayak <sibasisnayak07@gmail.com>
2020-09-04 23:52:00 +0530

    2,6 done

--- problem2/q2.py
+++ problem2/q2.py
@@ -1,1 +1,47 @@
 # Enter your code here
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-in','--infile',help="input")
+parser.add_argument('-out','--outfile',help="output")
+args = parser.parse_args()
+
+f = open(args.infile,"r")
+lines = f.read().splitlines()
+path = lines[0]
+
+f.close()
+
+dirlist = []
+
+for i in range(0,len(path)):
+	s = ""
+	if path[i] == '/':
+		i+=1
+		while (i<len(path) and path[i]!='/'):
+			s+=path[i]
+			i+=1
+	if(s =='.' or s==''):
+		continue
+	elif(s =='..'):
+		if(len(dirlist)!=0):
+			dirlist.pop()
+	else:
+		dirlist.append(s)
+
+canonpath=''
+
+if(len(dirlist)== 0):
+	canonpath+='/'
+else:
+	for i in range(0,len(dirlist)):
+		canonpath+='/'
+		canonpath+=dirlist[i]
+		
+
+f1 = open('outfile','w')
+f1.write(canonpath)
+f1.close()
+
+
--- problem6/q6.py
+++ problem6/q6.py
@@ -1,1 +1,14 @@
 # Enter your code here
+import functools
+
+def collapse(L):
+	if type(L[0]) is list:
+		L.insert(0,'\b')
+	s1 = functools.reduce(lambda a,b : a+' '+b if type(b) is str else a+' '+collapse(b),L)
+	if type(L[0]) is list:
+		L.pop(0)
+	return s1
+
+k =  [ ["this","is"], [ ["an", "interesting", "python"], ["programming", "exercise."] ] ]
+s = collapse(k)
+print(s)
