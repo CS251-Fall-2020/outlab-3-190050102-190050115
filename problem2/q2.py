0be353f08e42ccb23b6f9c34579ade2c20203f48
Sibasis Nayak <sibasisnayak07@gmail.com>
2020-09-06 20:23:36 +0530

    q2 fixed

--- problem2/q2.py
+++ problem2/q2.py
@@ -1,5 +1,6 @@
 # Enter your code here
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
 
@@ -13,31 +14,7 @@ path = lines[0]
 
 f.close()
 
-dirlist = []
-
-for i in range(0,len(path)):
-	s = ""
-	if path[i] == '/':
-		i+=1
-		while (i<len(path) and path[i]!='/'):
-			s+=path[i]
-			i+=1
-	if(s =='.' or s==''):
-		continue
-	elif(s =='..'):
-		if(len(dirlist)!=0):
-			dirlist.pop()
-	else:
-		dirlist.append(s)
-
-canonpath=''
-
-if(len(dirlist)== 0):
-	canonpath+='/'
-else:
-	for i in range(0,len(dirlist)):
-		canonpath+='/'
-		canonpath+=dirlist[i]
+canonpath = os.path.realpath(path)
 
 f1 = open(args.outfile,'w')
 f1.write(canonpath)
