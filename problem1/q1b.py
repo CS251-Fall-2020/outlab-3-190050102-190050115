544bc6f26c38bc623ecf19e087ecc4d1add2f4ea
Sahasra Ranjan <sahasra@cse.iitb.ac.in>
2020-09-06 14:06:14 +0530

    q1c +

--- problem1/q1b.py
+++ problem1/q1b.py
@@ -8,39 +8,26 @@ parser.add_argument("-m", "--message", help="Message file")
 args = parser.parse_args()
 
 def check(s, n):
-    a = re.findall("\d",s)
+    seqs = re.findall("\([-?\d\,]+\)",s)
+    a = []
+    b = []
     
-    # a = []
-    # b = []
-    # half = False
+    a = re.findall("\d+", seqs[0])
+    b = re.findall("\d+", seqs[1])
     
-    # for c in s:
-    #     if c == '#':
-    #         half = True
-    #     try:
-    #         if not half:
-    #             a.append(RingInt(int(c), n))
-    #         else:
-    #             b.append(RingInt(int(c), n))
-    #     except:
-    #         continue
+    sum_a = 0
+    for i in range(len(a)):
+        sum_a += (i+1)*int(a[i])
     
-    # left = RingInt(0, n)
-    # right = RingInt(0, n)
-    
-    # for i in range(len(a)):
-    #     left = left + RingInt(i+1, n) * a[i]
-    
-    # for i in range(len(b)):
-    #     right = right + RingInt(i+1, n) * b[i]
+    sum_b = 0
+    for i in range(len(b)):
+        sum_b += (i+1)*int(b[i])
         
-    # if left == right :
-    #     return "OK"
-    # else:
-    #     return "CORRUPTED"
-        
-        
-# print(check("$(1,-1,0)#(2,1,1,2)$", 3))
+    if sum_a%n == sum_b%n :
+        return "OK"
+    else:
+        return "CORRUPTED"
+
 
 file = open(args.message, "r")
 n = file.readline()
@@ -50,16 +37,4 @@ message = file.readline()
 codes = re.findall("\$\([\d\,]+\)\#\([\d\,]+\)\$", message)
 
 for code in codes:
-    print(code)
     print(check(code, int(n)))
-
-# print(codes)
-
-# RegEx: \$\(([0-9]+(,[0-9]+)+|[0-9])\)\#\(([0-9]+(,[0-9]+)+|[0-9])\)\$
-# RegEx: \$\((\d+(,\d+)+|\d)\)\#\((\d+(,\d+)+|\d)\)\$
-# RegEx: (,-?[0-9]+)+
-
-# 7
-# Hi. Long time since we met each other by dear siss!!! I was reading this book, bought it for $13.99. Found a line - When we were children,
-#  Hassan and I used to climb the $(1,0,0,0)#(3,6)$ trees in the driveway of my father's house and annoy our neighbours by reflecting sunlight onto their homes. 
-# This reminded me of our childhood. I wish to fly back to $(0,1,0,0,4)#(1)$ once this gets over and buy you and a $(1,1,1,1,1)#(4,4)$. #sis_goals. Signing off $$Sara$$.
