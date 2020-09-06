b3da96fbc3cb1d23b03b443ba2c640e5dc1d2aba
Sibasis Nayak <sibasisnayak@cse.iitb.ac.in>
2020-09-06 15:54:15 +0000

    6 fixed

--- problem6/q6.py
+++ problem6/q6.py
@@ -2,13 +2,13 @@
 import functools
 
 def collapse(L):
-	if type(L[0]) is list:
-		L.insert(0,'\b')
-	s1 = functools.reduce(lambda a,b : a+' '+b if type(b) is str else a+' '+collapse(b),L)
-	if type(L[0]) is list:
-		L.pop(0)
-	return s1
-
-k =  [ ["this","is"], [ ["an", "interesting", "python"], ["programming", "exercise."] ] ]
+	if type(L) is list:
+		s1 = list(map(collapse,L))
+		k = functools.reduce(lambda a,b :a+' '+b,s1)
+		return k
+	elif type(L) is str:
+		return L
+	
+k =  [ ["this","is"], [ [ ["okokok"],"an", "interesting", "python"], ["programming", "exercise."] ] ]
 s = collapse(k)
 print(s)
